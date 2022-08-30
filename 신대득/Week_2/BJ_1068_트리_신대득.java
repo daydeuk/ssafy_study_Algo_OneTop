2b809ac7bfe8dcb5a60841ea5e5004704b9d3bcd
daydeuk <88310641+daydeuk@users.noreply.github.com>
2022-08-30 20:17:02 +0900

    Update BJ_1068_트리_신대득.java

--- 신대득/Week_2/BJ_1068_트리_신대득.java
+++ 신대득/Week_2/BJ_1068_트리_신대득.java
@@ -1,102 +1,97 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-public class BJ_1068_트리_신대득 {
+/*
+ * 연결되어있는 트리들을 하나의 집합으로 묶어서, 몇개의 집합이 있는지 세는
+ * 서로소 문제
+ * Kruskal 알고리즘 사용 => find, Union
+ * 사이클은 어떻게 해결??????? => Union false
+ */
+public class BJ_4803_트리_신대득 {
 
-	static int N;
-	static int leafCount;
-
-	public static class Node {
-		int number;
-
-		Node left;
-		Node right;
-
-		int parentNumber;
-		Node parent;
-
-		public Node(int parentNumber) {
-			this.parentNumber = parentNumber;
-		}
-	}
+	static int N, M;
+	static int[] parents;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(in.readLine());
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");	
-		// root 노드
-		Node root = new Node(Integer.parseInt(st.nextToken()));
-		root.number = 0;
-		root.parent = null;
-		// root 노드 제외 n-1개 노드찾기
-		for (int i = 1; i < N; i++) {
-			Node curNode = root;
-			int parentNumber = Integer.parseInt(st.nextToken());
-			Node node = new Node(parentNumber);
-			node.number = i;
-			findNode(node, curNode);
-		}
-		int deleteNumber = Integer.parseInt(in.readLine());
+		StringBuilder sb = new StringBuilder();
+		int tc = 1;
+		while (true) {
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			if (N == 0 & M == 0)
+				break;
+			sb.append("Case ").append(tc).append(": ");
+			// make
+			parents = new int[N + 1];
+			for (int i = 1; i <= N; i++)
+				parents[i] = i;
 
-		if (deleteNumber == 0) {
-			System.out.printf("%d", leafCount);
-			return;
-		}
-		deleteNode(root, deleteNumber);
-		if (root != null)
-			findleaf(root);
-		System.out.printf("%d", leafCount);
-	}
-
-	public static void findleaf(Node curNode) {
-		if (curNode.left == null && curNode.right == null) {
-			leafCount++;
-			return;
-		}
-		if (curNode.left != null)
-			findleaf(curNode.left);
-		if (curNode.right != null)
-			findleaf(curNode.right);
-	}
-
-	public static void deleteNode(Node curNode, int deleteNumber) {
-		if (curNode.left != null) {
-			if (curNode.left.number == deleteNumber) {
-				curNode.left = null;
-				return;
+			// 입력
+			for (int m = 0; m < M; m++) {
+				st = new StringTokenizer(in.readLine(), " ");
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				union(from, to);
 			}
-		}
-		if (curNode.right != null) {
-			if (curNode.right.number == deleteNumber) {
-				curNode.right = null;
-				return;
+
+			// 유니온 파인드
+			int sum=0;
+			for(int i=1;i<=N;i++) {
+				if(find(i)==i) {
+					sum+=1;
+				}
 			}
+			
+			/*
+			Set s = new HashSet<Integer>();
+			for(int i=1;i<=N;i++) {
+				int root = find(i);
+				if(find(i)!=0)
+					s.add(root);
+			}
+				int sum = s.size();
+			*/
+		
+			
+			if (sum > 1) {
+				sb.append("A forest of ").append(sum).append(" trees.\n");
+			} else if (sum == 1) {
+				sb.append("There is one tree.\n");
+			} else if (sum == 0) {
+				sb.append("No trees.\n");
+			}
+			tc++;
 		}
-		if (deleteNumber < curNode.number) {
-			return;
-		}
-		if (curNode.left != null)
-			deleteNode(curNode.left, deleteNumber);
-		if (curNode.right != null)
-			deleteNode(curNode.right, deleteNumber);
+		System.out.printf(sb.toString());
+
 	}
 
-	public static void findNode(Node node, Node curNode) {
-		if (curNode.number == node.parentNumber) {
-			if (curNode.left == null) {
-				curNode.left = node;
-				return;
-			} else if (curNode.right == null) {
-				curNode.right = node;
-				return;
-			}
-		}
-		if (curNode.left != null)
-			findNode(node, curNode.left);
-		if (curNode.right != null)
-			findNode(node, curNode.right);
+	public static int find(int check) {
+		if (parents[check] == check)
+			return check;
+		return parents[check] = find(parents[check]);
 	}
 
+	public static boolean union(int from, int to) {
+		int aRoot = find(from);
+		int bRoot = find(to);
+		if (aRoot == bRoot || aRoot==0 || bRoot==0) { // 원래 같은 부모면.. 싸이클이면?
+			parents[aRoot] = 0;
+			parents[bRoot] = 0;
+			return false;
+		}
+		if(bRoot<aRoot) {
+			int tmp=aRoot;
+			aRoot=bRoot;
+			bRoot=tmp;
+		}
+		parents[bRoot] = aRoot;
+		return true;
+	}
 }
