078a2c57ff05903b867393b316a9557bf5cf6730
bulnabang99 <bulnabang99@gmail.com>
2022-09-21 01:41:21 +0900

    hash

---
+++ 이원석/22.09.14 ~ 22.09.21/PG_베스트앨범_이원석.java
@@ -0,0 +1,15 @@
+package com.ssafy._2022_09.day_220920;
+
+public class PG_베스트앨범_이원석 {
+    public static void main(String[] args) {
+        solution(new String[]{"classic", "pop", "classic", "classic", "pop"},
+                new int[]{500, 600, 150, 800, 2500});
+    }
+
+    public static int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+
